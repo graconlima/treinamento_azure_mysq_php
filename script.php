44bf8130308aa153436ee9175759a45b863cffff
graconlima <94612163+graconlima@users.noreply.github.com>
2024-07-15 16:52:05 -0300

    Update script.php

--- script.php
+++ script.php
@@ -3,56 +3,7 @@
 echo 
 "<html>
 <head>
-<style media='screen'>
-*,
-*:before,
-*:after{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
-body{
-    background-color: #080710;
-}
-.background{
-    width: 430px;
-    height: 520px;
-    position: absolute;
-    transform: translate(-50%,-50%);
-    left: 50%;
-    top: 50%;
-}
-.background .shape{
-    height: 200px;
-    width: 200px;
-    position: absolute;
-    border-radius: 50%;
-}
-.shape:first-child{
-    background: linear-gradient(
-        #1845ad,
-        #23a2f6
-    );
-    left: -80px;
-    top: -80px;
-}
-.shape:last-child{
-    background: linear-gradient(
-        to right,
-        #ff512f,
-        #f09819
-    );
-    right: -30px;
-    bottom: -80px;
-}
-label{
-    color: white;
-    display: block;
-    margin-top: 30px;
-    font-size: 16px;
-    font-weight: 500;
-}
-</style>
+<link rel="stylesheet" type="text/css" href="estilo.css" />
 </head>
 <body>
 <label for='log'>
